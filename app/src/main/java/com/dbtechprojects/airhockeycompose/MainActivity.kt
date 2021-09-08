a9345a3a1923508d418f45bb5058d8a3c9ff4a4f
Daniel Butler <danielmbutler@hotmail.co.uk>
2021-09-08 17:33:29 +0100

    implemented hitting the ball up

--- app/src/main/java/com/dbtechprojects/airhockeycompose/MainActivity.kt
+++ app/src/main/java/com/dbtechprojects/airhockeycompose/MainActivity.kt
@@ -8,18 +8,16 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.dbtechprojects.airhockeycompose.ui.gameScreen.GameBorder
+import com.dbtechprojects.airhockeycompose.ui.gameScreen.GameBoard
 import com.dbtechprojects.airhockeycompose.ui.theme.AirHockeyComposeTheme
 
 class MainActivity : ComponentActivity() {
 
     private val gameModeState = mutableStateOf(false)
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,7 +30,6 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Preview
 @Composable
 fun MainScreen(gameModeState: MutableState<Boolean>) {
     Surface(
@@ -41,7 +38,7 @@ fun MainScreen(gameModeState: MutableState<Boolean>) {
     ) {
 
         Column(Modifier.padding(10.dp)) {
-            GameBorder(gameModeState)
+            GameBoard(gameModeState)
 
         }
 
--- app/src/main/java/com/dbtechprojects/airhockeycompose/ui/gameScreen/Composables.kt
+++ app/src/main/java/com/dbtechprojects/airhockeycompose/ui/gameScreen/Composables.kt
@@ -1,8 +1,11 @@
 package com.dbtechprojects.airhockeycompose.ui.gameScreen
 
-
 import android.util.Log
 import android.util.Range
+import androidx.compose.animation.core.AnimationSpec
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
@@ -42,97 +45,124 @@ fun GameTitle(text: String) {
 }
 
 @Composable
-fun GameBorder(gameModeState: MutableState<Boolean>) {
-    var offsetX by remember { mutableStateOf(450f) }
-    var offsetY by remember { mutableStateOf(1780f) }
+fun GameBoard(gameModeState: MutableState<Boolean>) {
+    var playerOffsetX by remember { mutableStateOf(450f) }
+    var playerOffsetY by remember { mutableStateOf(1780f) }
+    var ballOffsetX by remember { mutableStateOf(461f) }
+    var ballOffsetY by remember { mutableStateOf(958f) }
+    var collision by remember {
+        mutableStateOf(false)
+    }
 
-    Box(
-        Modifier
-            .border(6.dp, color = Color.White, shape = RectangleShape)
-            .background(Color.Gray)
-            .fillMaxSize()
+    val hasPlayerHitBall: Boolean =
+        Range.create(ballOffsetX - 150f, ballOffsetX + 150f).contains(playerOffsetX) &&
+                Range.create(ballOffsetY - 150f, ballOffsetY + 150f).contains(playerOffsetY)
+
+    Log.d(
+        "GameBoard", "playerXOffset: $playerOffsetX playerYOffset: ${playerOffsetY}," +
+                " ballOffsetX ${ballOffsetX}, ballOffsetY ${ballOffsetY}, collision: $hasPlayerHitBall"
     )
-    {
 
-        Column() {
-            Canvas(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(20.dp)
-                    .pointerInput(Unit) {
-                        /*
-                        handle dragging player counter, the below logic keeps the playing within the game
-                        boundaries, as the values are mutable we are continuously keeping track of them and will update
-                        the players location unless the drag event is within the boundary
-                         */
-                        detectDragGestures { change, dragAmount ->
-                            change.consumeAllChanges()
-                            // these range conditions confirm that the finger is placed within 100f of the players puck
-                            if (Range
-                                    .create(offsetX - 100f, offsetX + 100f)
-                                    .contains(change.position.x) && Range
-                                    .create(
-                                        offsetY - 100f,
-                                        offsetY + 100f
-                                    )
-                                    .contains(change.position.y)
-                            ) {
-                                if (offsetX < 805f && offsetX > 160f) {
-                                    //player is within boundary so update location
-                                    offsetX += dragAmount.x
-                                } else if (offsetX > 805f && dragAmount.x < 0) {
-                                    // player is almost at the right most edge of boundary so only accept drags to the left
-                                    offsetX += dragAmount.x
-                                } else if (offsetX < 160f && dragAmount.x > 0) {
-                                    // player is almost at the left most edge of boundary so only accept drags to the right
-                                    offsetX += dragAmount.x
-                                }
+    if (hasPlayerHitBall){
+        collision = true
+    }
+    val ballMovementYAxis by animateFloatAsState(
+        //if game has not started or the game is in reverse mode then move enemies back to enemy box
+        targetValue =  if (!collision) {
+            ballOffsetY
+        } else {
+            ballOffsetY - 750f
+        },
+        animationSpec = tween(4000, easing = LinearEasing),
+        finishedListener = {
+        }
+    )
 
-                                // handle Vertical dragging
-                                if (offsetY > 1050f && offsetY < 1790f) {
-                                    offsetY += dragAmount.y
-                                }
-                                else if (offsetY > 1790f && dragAmount.y < 0) {
-                                    offsetY += dragAmount.y
-                                }
-                                else if (offsetY < 1050f && dragAmount.y > 0) {
-                                    offsetY += dragAmount.y
-                                }
+        Box(
+            Modifier
+                .border(6.dp, color = Color.White, shape = RectangleShape)
+                .background(Color.Gray)
+                .fillMaxSize()
+        )
+        {
 
+            Column() {
+                Canvas(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(20.dp)
+                        .pointerInput(Unit) {
+                            /*
+                            handle dragging player counter, the below logic keeps the playing within the game
+                            boundaries, as the values are mutable we are continuously keeping track of them and will update
+                            the players location unless the drag event is within the boundary
+                             */
+                            detectDragGestures { change, dragAmount ->
+                                change.consumeAllChanges()
+                                // these range conditions confirm that the finger is placed within 100f of the players puck
+                                if (Range
+                                        .create(playerOffsetX - 100f, playerOffsetX + 100f)
+                                        .contains(change.position.x) && Range
+                                        .create(
+                                            playerOffsetY - 100f,
+                                            playerOffsetY + 100f
+                                        )
+                                        .contains(change.position.y)
+                                ) {
+                                    if (playerOffsetX < 805f && playerOffsetX > 160f) {
+                                        //player is within boundary so update location
+                                        playerOffsetX += dragAmount.x
+                                    } else if (playerOffsetX > 805f && dragAmount.x < 0) {
+                                        // player is almost at the right most edge of boundary so only accept drags to the left
+                                        playerOffsetX += dragAmount.x
+                                    } else if (playerOffsetX < 160f && dragAmount.x > 0) {
+                                        // player is almost at the left most edge of boundary so only accept drags to the right
+                                        playerOffsetX += dragAmount.x
+                                    }
+
+                                    // handle Vertical dragging
+                                    if (playerOffsetY > 1050f && playerOffsetY < 1790f) {
+                                        playerOffsetY += dragAmount.y
+                                    } else if (playerOffsetY > 1790f && dragAmount.y < 0) {
+                                        playerOffsetY += dragAmount.y
+                                    } else if (playerOffsetY < 1050f && dragAmount.y > 0) {
+                                        playerOffsetY += dragAmount.y
+                                    }
+
+                                }
                             }
                         }
-                    }
-            ) {
-                val height = this.size.height
-                val width = this.size.width
+                ) {
+                    val height = this.size.height
+                    val width = this.size.width
 
-                drawGameBoard(height, width)
+                    drawGameBoard(height, width)
 
-                // ball
-                drawCircle(
-                    color = Color.Black,
-                    radius = 40f,
-                    center = Offset(width / 2, height / 2),
+                    // ball
+                    drawCircle(
+                        color = Color.Black,
+                        radius = 40f,
+                        center = Offset(ballOffsetX, ballMovementYAxis),
 
+                        )
+                    // pucks
+                    drawCircle(
+                        color = Color.Red,
+                        radius = 100f,
+                        center = Offset(width / 2, 100f),
                     )
-                // pucks
-                drawCircle(
-                    color = Color.Red,
-                    radius = 100f,
-                    center = Offset(width / 2, 100f),
-                )
-                //Offset(width/2, height - 100f),
-                drawCircle(
-                    color = Color.Blue,
-                    radius = 100f,
-                    center = Offset(offsetX, offsetY)
-                )
+                    //Offset(width/2, height - 100f),
+                    drawCircle(
+                        color = Color.Blue,
+                        radius = 100f,
+                        center = Offset(playerOffsetX, playerOffsetY)
+                    )
+                }
+            }
+            if (!gameModeState.value) {
+                GameMenu { gameModeState.value = true }
             }
         }
-        if (!gameModeState.value) {
-            GameMenu { gameModeState.value = true }
-        }
-    }
 }
 
 @Composable
