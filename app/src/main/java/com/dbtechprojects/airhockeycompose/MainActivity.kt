2ff005b4be99aa8f7cb09679a9ec3f7d5b72c0c7
Daniel Butler <danielmbutler@hotmail.co.uk>
2021-09-08 11:49:52 +0100

    adding game title and border

--- app/src/main/java/com/dbtechprojects/airhockeycompose/MainActivity.kt
+++ app/src/main/java/com/dbtechprojects/airhockeycompose/MainActivity.kt
@@ -3,13 +3,14 @@ package com.dbtechprojects.airhockeycompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.dbtechprojects.airhockeycompose.ui.gameScreen.GameBorder
 import com.dbtechprojects.airhockeycompose.ui.gameScreen.GameTitle
 import com.dbtechprojects.airhockeycompose.ui.theme.AirHockeyComposeTheme
 
@@ -19,12 +20,22 @@ class MainActivity : ComponentActivity() {
         setContent {
             AirHockeyComposeTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background, modifier = Modifier.fillMaxSize()) {
-                    GameTitle(text = "Air Hockey Compose")
-                }
+                MainScreen()
             }
         }
     }
 }
 
-
+@Preview
+@Composable
+fun MainScreen() {
+    Surface(
+        color = MaterialTheme.colors.background, modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Column {
+            GameTitle(text = "Air Hockey Compose")
+            GameBorder()
+        }
+    }
+}
--- app/src/main/java/com/dbtechprojects/airhockeycompose/ui/gameScreen/Composables.kt
+++ app/src/main/java/com/dbtechprojects/airhockeycompose/ui/gameScreen/Composables.kt
@@ -1,31 +1,47 @@
 package com.dbtechprojects.airhockeycompose.ui.gameScreen
 
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
-fun GameTitle(text: String){
+fun GameTitle(text: String) {
     Row(
         horizontalArrangement = Arrangement.Center,
         modifier = Modifier
             .fillMaxWidth()
             .padding(6.dp)
     ) {
-        Text(text = text,
+        Text(
+            text = text,
             color = Color.White,
             textAlign = TextAlign.Center,
             fontSize = 32.sp,
-            fontWeight = FontWeight.Bold,)
+            fontWeight = FontWeight.Bold,
+        )
+    }
+}
+
+@Composable
+fun GameBorder() {
+    Box(
+        Modifier
+            .border(6.dp, color = Color.White, shape = RectangleShape)
+            .background(Color.Gray)
+            .fillMaxSize()
+    )
+    {
+        Canvas(modifier = Modifier.fillMaxSize(), onDraw = )
     }
 }
