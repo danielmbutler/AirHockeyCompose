f265aa82576169bd0511d77b0a407969b4b8ffba
Daniel Butler <danielmbutler@hotmail.co.uk>
2021-09-16 09:42:47 +0100

    adding SocketHandler.kt

---
+++ app/src/main/java/com/dbtechprojects/airhockeycompose/network/SocketHandler.kt
@@ -0,0 +1,41 @@
+package com.dbtechprojects.airhockeycompose.network
+
+import io.socket.client.IO
+import io.socket.client.Socket
+import java.net.URISyntaxException
+
+
+//https://medium.com/@thushenarriyam/socket-io-connection-on-android-kotlin-to-node-js-server-71b218c160c9
+
+object SocketHandler {
+
+    lateinit var mSocket: Socket
+
+    @Synchronized
+    fun setSocket() {
+        try {
+// "http://10.0.2.2:3000" is the network your Android emulator must use to join the localhost network on your computer
+// "http://localhost:3000/" will not work
+// If you want to use your physical phone you could use the your ip address plus :3000
+// This will allow your Android Emulator and physical device at your home to connect to the server
+            mSocket = IO.socket("http://10.0.2.2:3000")
+        } catch (e: URISyntaxException) {
+
+        }
+    }
+
+    @Synchronized
+    fun getSocket(): Socket {
+        return mSocket
+    }
+
+    @Synchronized
+    fun establishConnection() {
+        mSocket.connect()
+    }
+
+    @Synchronized
+    fun closeConnection() {
+        mSocket.disconnect()
+    }
+}
